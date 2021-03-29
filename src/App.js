2979c5fa5c0014827a934b1c1ba79b62ec2dcbb3
Vamsi K <41518395+VAMSI08@users.noreply.github.com>
2021-03-29 20:11:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,10 +12,9 @@ function App() {
 
   const newsApi = async () => {
     try {
-      const proxyUrl = "https://cors-anywhere.herokuapp.com/";
 
       const news = await axios.get(
-        `${proxyUrl}https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.REACT_APP_API_KEY}&pageSize=${loadMore}&category=${category}`
+        `https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.REACT_APP_API_KEY}&pageSize=${loadMore}&category=${category}`
       );
       setNewsArray(news.data.articles);
       setNewsResults(news.data.totalResults);
